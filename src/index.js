af728acb529c0fc40a1c0b73f29ff03f1ad2048e
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-03-22 19:58:07 +0300

    init:" start expression-calculator task"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,77 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
+const operations = new Map([
+    ["+", (x,y) => x + y],
+    ["-", (x,y) => x - y],
+    ["*", (x,y) => x*y],
+    ["/", (x,y) => {
+        if( y === 0)
+            throw "TypeError: Division by zero.";
+        return x / y;
+    }]
+]);
 
 function expressionCalculator(expr) {
-    // write your solution here
+    expr = expr.replace(/ /g, "");
+
+    if(expr.replace(/[\d+\-*/)]/g, "").length !== expr.replace(/[\d+\-*/(]/g, "").length)
+        throw "ExpressionError: Brackets must be paired";
+        
+    return calculate(expr.split("").join(" ").replace(/\b \b/g, "").split(" ")).value;
+}
+
+
+function calculate(arr){
+    var bracketsStack = [];
+    var multiplyStack = [];
+    var sumStack = [];
+    var fBracketsIndex = arr.findIndex(x => x === "(");
+    var lBracketsIndex = arr.findIndex(x => x === ")");
+    var rest = [];
+
+    while(fBracketsIndex >= 0 && fBracketsIndex < lBracketsIndex)
+    {    
+        let result = calculate(arr.slice(fBracketsIndex + 1, arr.length));
+        bracketsStack = arr.slice(0, fBracketsIndex);
+        bracketsStack.push(result.value);
+        bracketsStack = bracketsStack.concat(result.rest);
+        lBracketsIndex = bracketsStack.findIndex(x => x === ")");
+        fBracketsIndex = bracketsStack.findIndex(x => x === "(");
+        arr = bracketsStack;
+    }
+
+    if(bracketsStack.length === 0)
+        bracketsStack = arr;
+    
+    if(lBracketsIndex > 0)
+    {   
+        rest = bracketsStack.slice(lBracketsIndex+1, bracketsStack.length);
+        bracketsStack.length = lBracketsIndex;
+    }
+
+    for(let i = 0; i < bracketsStack.length; i++)
+    {
+        if(bracketsStack[i] === "*" || bracketsStack[i] === "/")
+        {
+            let func = operations.get(bracketsStack[i]);
+            multiplyStack.push(func(multiplyStack.pop(), Number(bracketsStack[i+1])));
+            i++;
+        }
+        else    
+            multiplyStack.push(operations.has(bracketsStack[i]) ? bracketsStack[i] : Number(bracketsStack[i]));
+    }
+
+    for(let i = 0; i < multiplyStack.length; i++)
+    {
+        if(multiplyStack[i] === "+" || multiplyStack[i] === "-")
+        {
+            let func = operations.get(multiplyStack[i]);
+            sumStack.push(func(sumStack.pop(), multiplyStack[i+1]));
+            i++;
+        }
+        else
+            sumStack.push(Number(multiplyStack[i]));
+    }
+
+    return {value : sumStack.pop(), rest};
 }
 
 module.exports = {
